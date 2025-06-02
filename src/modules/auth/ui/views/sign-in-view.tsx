36f6ac352f8f39f20c05463824a7dadd80590902
Adam_Mikulas <miku.adam@gmail.com>
2025-06-02 20:34:31 +0200

    feat: add dashboard sidebar with user profile and navigation menu

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -132,7 +132,7 @@ export const SignInView = () => {
             </form>
           </Form>
 
-          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="May.AI Image" className="h-[92px] w-[92px]" />
             <p className="text-2xl font-semibold text-white">May.AI</p>
           </div>
